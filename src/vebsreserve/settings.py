bbf3ab8d0048c2152a8e44192f1a1aee859ef226
gotpredictions <eswar.vandanapu@gnana.com>
2012-11-25 10:15:17 -0800

    Login data and dev form for login.

--- src/vebsreserve/settings.py
+++ src/vebsreserve/settings.py
@@ -1,4 +1,7 @@
 # Django settings for vebsreserve project.
+import os
+
+PROJECT_DIR = os.path.abspath(os.path.join(__file__,'..','..','..'))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -108,6 +111,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_DIR,'templates'),
 )
 
 INSTALLED_APPS = (
